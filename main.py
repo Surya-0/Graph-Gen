d396217aea8d1f1dc5317238478fb0cd3ecac905
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-09-24 21:47:00 +0530

    My graph app

--- main.py
+++ main.py
@@ -11,7 +11,31 @@ def main():
 
     if page == "Home":
         st.title("Graph Analysis App")
-        st.write("Welcome to the Graph Analysis App. Use the sidebar to navigate between different functionalities.")
+        st.write("""
+        ### Welcome to the Graph Generation App
+
+        This app creates **dynamic supply chain graphs** using advanced **random generation techniques**. Here’s how the process works:
+
+        #### Node Creation and Structure
+        - We start with a **base of 26 nodes**, including one **Business Group**, multiple **Product Families**, and their associated **Product Offerings**.
+        - From there,we can add how many ever nodes we want based on our requirement by entering into the **user input** placeholder.
+
+        #### Randomized Node and Link Generation
+        Our unique graph generation process employs **two key randomization steps**:
+
+        1. **Density Factor Influence**:
+           - Each **Product Offering** is assigned a **Density Factor** (a value between 0 and 1) that determines the likelihood of forming a **link**. A higher density factor means more connections, while a lower density factor limits links.
+           - This first randomization ensures that the structure of the graph is unique and adaptive to each node.
+
+        2. **Poisson/Normal Distribution**:
+           - Once the possibility of a link is determined, the number of **Modules** and **Parts** linked to each **Product Offering** is calculated using **Poisson** or **Normal distribution**. This adds an element of **natural randomness** in the number of nodes generated, mimicking real-world supply chain dynamics.
+           - The **Density Factor** also governs the number of connections added. High-density offerings receive more links, while low-density offerings generate fewer.
+
+        Through this process, the graph grows organically, reflecting realistic and varied supply chain relationships. 🎯
+
+        Please start exploring and generating your own **supply chain network** now!!
+        """)
+
     elif page == "Graph Generation":
         graph_generation_page()
     elif page == "Graph Analysis":
@@ -24,4 +48,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
