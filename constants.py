50ad08b8971a331e841bde5cf04b15c32fe47f76
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-09-24 20:30:18 +0530

    My graph app

---
+++ constants.py
@@ -0,0 +1,40 @@
+BUSINESS_GROUP = {'BG001': 'Etch'}
+PRODUCT_FAMILIES = [
+    {'Family_ID': 'PF001', 'Family_Name': 'Kyo'},
+    {'Family_ID': 'PF002', 'Family_Name': 'Coronus'},
+    {'Family_ID': 'PF003', 'Family_Name': 'Flex'},
+    {'Family_ID': 'PF004', 'Family_Name': 'Versys Metal'}
+]
+PRODUCT_OFFERINGS = [
+    {'Offering_ID': 'PO001', 'Offering_Name': 'Versys® Kyo®', 'Family_ID': 'PF001'},
+    {'Offering_ID': 'PO002', 'Offering_Name': 'Versys® Kyo® C Series', 'Family_ID': 'PF001'},
+    {'Offering_ID': 'PO003', 'Offering_Name': 'Kyo® C Series', 'Family_ID': 'PF001'},
+    {'Offering_ID': 'PO004', 'Offering_Name': 'Kyo® E Series', 'Family_ID': 'PF001'},
+    {'Offering_ID': 'PO005', 'Offering_Name': 'Kyo® F Series', 'Family_ID': 'PF001'},
+    {'Offering_ID': 'PO006', 'Offering_Name': 'Kyo® G Series', 'Family_ID': 'PF001'},
+    {'Offering_ID': 'PO007', 'Offering_Name': 'Coronus®', 'Family_ID': 'PF002'},
+    {'Offering_ID': 'PO008', 'Offering_Name': 'Coronus® HP', 'Family_ID': 'PF002'},
+    {'Offering_ID': 'PO009', 'Offering_Name': 'Coronus® DX', 'Family_ID': 'PF002'},
+    {'Offering_ID': 'PO010', 'Offering_Name': 'Exelan® Flex®', 'Family_ID': 'PF003'},
+    {'Offering_ID': 'PO011', 'Offering_Name': 'Exelan® Flex45™', 'Family_ID': 'PF003'},
+    {'Offering_ID': 'PO012', 'Offering_Name': 'Flex® D Series', 'Family_ID': 'PF003'},
+    {'Offering_ID': 'PO013', 'Offering_Name': 'Flex® E Series', 'Family_ID': 'PF003'},
+    {'Offering_ID': 'PO014', 'Offering_Name': 'Flex® F Series', 'Family_ID': 'PF003'},
+    {'Offering_ID': 'PO015', 'Offering_Name': 'Flex® G Series', 'Family_ID': 'PF003'},
+    {'Offering_ID': 'PO016', 'Offering_Name': 'Flex® H Series', 'Family_ID': 'PF003'},
+    {'Offering_ID': 'PO017', 'Offering_Name': 'Versys® Metal', 'Family_ID': 'PF004'},
+    {'Offering_ID': 'PO018', 'Offering_Name': 'Versys® Metal45™', 'Family_ID': 'PF004'},
+    {'Offering_ID': 'PO019', 'Offering_Name': 'Versys® Metal L', 'Family_ID': 'PF004'},
+    {'Offering_ID': 'PO020', 'Offering_Name': 'Versys® Metal M', 'Family_ID': 'PF004'},
+    {'Offering_ID': 'PO021', 'Offering_Name': 'Versys® Metal N', 'Family_ID': 'PF004'}
+]
+
+
+COLOR_MAP = {
+    'business_group': '#FF9999',
+    'family': '#66B2FF',
+    'offering': '#99FF99',
+    'module': '#FFCC99',
+    'make': '#FF99CC',
+    'purchase': '#99CCFF'
+}
